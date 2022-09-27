96fd244bbde0fd80ba5d20d1fad8f1a9b9c1cee9
tudou <525589458@qq.com>
2022-09-27 23:48:00 +0800

    更新链接

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,9 +3,38 @@ module.exports = {
   title: '博客',
   dest: './build',
   theme: 'reco',
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  },
   themeConfig: {
     type: 'blog',
     subSidebar: 'auto',
-    authorAvatar: '/avatar.jpg'
+    authorAvatar: '/avatar.jpg',
+    nav: [
+      {
+        text: 'home',
+        icon:"reco-home",
+        link: '/',
+      },
+      {
+        text: 'github',
+        icon:"reco-github",
+        link: 'https://github.com/zhoubin-datareachable/blog',
+      },
+      {
+        text: '留言板',
+        icon:"reco-suggestion",
+        link: 'https://support.qq.com/product/435903',
+      },
+      { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' }
+    ],
+    record: 'ICP 备案文案',
+    recordLink: 'ICP 备案指向链接',
+    cyberSecurityRecord: '公安部备案文案',
+    cyberSecurityLink: '公安部备案指向链接',
+    startYear: '2021',
+    author: '土豆'
   },
 };
--- docs/README.md
+++ docs/README.md
@@ -2,5 +2,5 @@
 home: true
 heroText: 哈喽，好久不见
 tagline: 想法 + 实践 = Bugs
-bgImageStyle: { height: 200px }
+bgImageStyle: { height: 300px }
 ---
