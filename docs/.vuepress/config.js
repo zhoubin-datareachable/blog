0cf72cd3228a5ff8a71e090934edcbd72c3f94ac
zhoubin-datareachable <bin.zhou@datareachable.com>
2022-09-29 09:34:24 +0800

    类型选择

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,40 +1,40 @@
 module.exports = {
-  base: '/',
-  title: '博客',
-  dest: './build',
-  theme: 'reco',
+  base: "/",
+  title: "博客",
+  dest: "./build",
+  theme: "reco",
   locales: {
-    '/': {
-      lang: 'zh-CN'
-    }
+    "/": {
+      lang: "zh-CN",
+    },
   },
   themeConfig: {
-    type: 'blog',
-    subSidebar: 'auto',
-    authorAvatar: '/avatar.jpg',
+    type: "blog",
+    subSidebar: "auto",
+    authorAvatar: "/avatar.jpg",
     nav: [
       {
-        text: 'home',
-        icon:"reco-home",
-        link: '/',
+        text: "home",
+        icon: "reco-home",
+        link: "/",
       },
       {
-        text: 'github',
-        icon:"reco-github",
-        link: 'https://github.com/zhoubin-datareachable',
+        text: "github",
+        icon: "reco-github",
+        link: "https://github.com/zhoubin-datareachable",
       },
       {
-        text: '留言板',
-        icon:"reco-suggestion",
-        link: 'https://support.qq.com/product/435903',
+        text: "留言板",
+        icon: "reco-suggestion",
+        link: "https://support.qq.com/product/435903",
       },
-      { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' }
+      { text: "TimeLine", link: "/timeline/", icon: "reco-date" },
     ],
-    record: 'ICP 备案文案',
-    recordLink: 'ICP 备案指向链接',
-    cyberSecurityRecord: '公安部备案文案',
-    cyberSecurityLink: '公安部备案指向链接',
-    startYear: '2021',
-    author: '土豆'
+    record: "ICP 备案文案",
+    recordLink: "ICP 备案指向链接",
+    cyberSecurityRecord: "公安部备案文案",
+    cyberSecurityLink: "公安部备案指向链接",
+    startYear: "2021",
+    author: "土豆",
   },
 };
